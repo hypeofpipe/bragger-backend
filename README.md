0295476132fd30706039656fdf0d19504385d2e8
hypeofpipe <hypeofpipe@gmail.com>
2019-04-16 23:38:36 +0200

    Working on DB

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # bragger-backend
 
+## Database
+
+Use Docker.
+
+1. Open a terminal.
+2. `docker pull postgres`
+3. `docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 postgres`
+4. `docker exec -it $(docker ps | grep 'pg-docker' | cut -c 1-12) /bin/bash`
+
 ## Start
 `yarn start`
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+import { schema } from './MyGraphQLSchema';
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { postgraphile } = require("postgraphile");
+const { postgraphile } = require('postgraphile');
+require('dotenv').config();
 
 const app = express();
 
-// app.use('/graphql', graphqlHTTP({
-//   schema: MyGraphQLSchema,
-//   graphiql: true
-// }));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
 
-app.use(postgraphile(process.env.DATABASE_URL || "postgres:///"));
+app.use(
+  postgraphile(process.env.DATABASE_URL || 'postgres:///', 'public', {
+    watchPg: true,
+  }),
+);
 
+console.log('listening on 3000');
 app.listen(3000);
